7e0f536c37b615198ba52ed9bc4f22cd86d4f38d
gbiggios <168198090+gbiggios@users.noreply.github.com>
2024-12-16 00:50:52 -0300

    Update run.py

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     
     # Elimina debug=True para producción; úsalo solo en desarrollo
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
